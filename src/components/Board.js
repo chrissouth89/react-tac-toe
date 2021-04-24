b31d07e613d7d3b3283ad681f524f1bda0474ca2
chris <chrissouth89@gmail.com>
2021-04-24 09:18:09 -0500

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import Square from "./Square";
+
+function Board(props) {
+  return (
+    <div classname="board">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
+
+export default Board;
